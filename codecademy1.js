f9f00c25d536c7028019c3a3039233b17db06038
Wagnerf <wagnerferreira@outlook.com>
2020-07-17 17:54:26 -0300

    Minor alterarion

--- codecademy1.js
+++ codecademy1.js
@@ -1,14 +1,14 @@
 var filme = prompt("Escolha um filme:");
 
 var getReview = function (movie) {
-    if (movie === "Toy Story 2") {
-        alert("Grande história. Mineiro ruim.");
-    } else if (movie === "Procuraodo Nemo") {
-        alert("Animação legal e tartarugas engracadas.");
-    } else if (movie === "O Rei Leao") {
-        alert("Grandes musicas.");
+    if (movie === "Star Wars") {
+        alert("May the force be with you");
+    } else if (movie === "Mad Max") {
+        alert("The road warrior");
+    } else if (movie === "Aliens") {
+        alert("I´m a passenger...");
     } else {
-        alert("Eu nao sei!");
+        alert("Em breve, mais opções...");
     };
 };
 
